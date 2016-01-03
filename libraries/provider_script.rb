405f20e2a9e5222bf501336a33721c1a0f2ac73d
Virender Khatri <vir.khatri@gmail.com>
2016-01-03 20:41:31 +0530

    add resource script to handle scripts in the cookbook #9

---
+++ libraries/provider_script.rb
@@ -0,0 +1,52 @@
+class Chef
+  class Provider
+    # provides consul_script
+    class ConsulScript < Chef::Provider
+      provides :consul_script if respond_to?(:provides)
+
+      def initialize(*args)
+        super
+      end
+
+      def whyrun_supported?
+        true
+      end
+
+      def load_current_resource
+        true
+      end
+
+      def action_create
+        new_resource.updated_by_last_action(script_file)
+      end
+
+      def action_delete
+        new_resource.updated_by_last_action(script_file)
+      end
+
+      protected
+
+      def script_file
+        if new_resource.script_content
+          t = Chef::Resource::File.new("consul_script_#{new_resource.name}", run_context)
+          t.content new_resource.script_content
+        elsif new_resource.cookbook_file
+          t = Chef::Resource::CookbookFile.new("consul_script_#{new_resource.name}", run_context)
+          t.source new_resource.cookbook_file if new_resource.cookbook_file
+          t.cookbook new_resource.cookbook if new_resource.cookbook
+        elsif new_resource.cookbook_template
+          t = Chef::Resource::Template.new("consul_script_#{new_resource.name}", run_context)
+          t.source new_resource.cookbook_template if new_resource.cookbook_template
+          t.variables new_resource.template_variables if new_resource.template_variables
+          t.cookbook new_resource.cookbook if new_resource.cookbook
+        else
+          fail 'must define one of resource attribute - :script_content, :cookbook_template or :cookbook_file'
+        end
+        t.path ::File.join(node['consul']['scripts_dir'], new_resource.name)
+        t.mode 0755
+        t.run_action new_resource.action
+        t.updated?
+      end
+    end
+  end
+end
---
+++ libraries/resource_script.rb
@@ -0,0 +1,52 @@
+require 'chef/resource'
+
+class Chef
+  class Resource
+    # provides consul_script
+    class ConsulScript < Chef::Resource
+      identity_attr :name
+
+      def initialize(name, run_context = nil)
+        super
+        @resource_name = :consul_script
+        @provides = :consul_script
+        @provider = Chef::Provider::ConsulScript
+        @action = :create
+        @allowed_actions = [:create, :delete, :nothing]
+        @name = name
+      end
+
+      def cookbook_template(arg = nil)
+        set_or_return(
+          :cookbook_template, arg,
+          :kind_of => String,
+          :default => nil
+        )
+      end
+
+      def cookbook_file(arg = nil)
+        set_or_return(
+          :cookbook_file, arg,
+          :kind_of => String,
+          :default => nil
+        )
+      end
+
+      def cookbook(arg = nil)
+        set_or_return(
+          :cookbook, arg,
+          :kind_of => String,
+          :default => nil
+        )
+      end
+
+      def script_content(arg = nil)
+        set_or_return(
+          :script_content, arg,
+          :kind_of => String,
+          :default => nil
+        )
+      end
+    end
+  end
+end
